9b8b23bdcf39cbd44a25dca5bfa978c335110262
Ali Shamsian <Alishamsian504@gmail.com>
2022-02-13 00:42:09 +0330

    final

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,9 @@
-
-import products from "../assets/JsonData/products.json";
+// import products from "../assets/JsonData/products.json";
 import Table from "../components/table/Table";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useState , useEffect } from "react";
+
+axios.defaults.baseURL = "https://my.api.mockaroo.com";
 
 const productTableHead = [
   "",
@@ -10,39 +11,58 @@ const productTableHead = [
   "قیمت",
   "موجودی",
   "خریداری شده",
-  "دسته بندی"
+  "دسته بندی",
 ];
 
-const renderHead = (item, index)=> <th key={index}>{item}</th>
+const renderHead = (item, index) => <th key={index}>{item}</th>;
 
 
+const renderBody = (item, index) => (
+  <tr key={index}>
+    <td>{item.id}</td>
+    <td>{item.first_name}</td>
+    <td>{item.email}</td>
+    <td>{item.ip_address}</td>
+  </tr>
+);
 
-
-const renderBody = (item, index)=>(
-    <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.title}</td>
-        <td>{item.price}</td>
-        <td>{item.stock}</td>
-        <td>{item.sold}</td>
-        <td>{item.category}</td>
-        
-    </tr>
-)
 
 const Products = () => {
+  //   const [data, setData] = useState([])
 
-  const [data, setData] = useState([])
+  //   const fetchData = () => {
+  //     axios
+  //         .get('/posts')
+  //         .then((res) => {
+  //             console.log(res.data);
+  //             setData(res.data)
+  //         })
+  //         .catch((err) => {
+  //             console.log(err);
+  //         });
+  // };
+
+  // useEffect(() => {
+  //     fetchData();
+  // }, []);
+
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    axios.get("https://api.sampleapis.com/beers/ale")
-    .then((res) =>{
-      setData(res.data)
-  
-      console.log(data)
-    }).catch();
-  }, [])
-  
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get("/pruducts.json?key=614e2dd0");
+        setData(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    };
 
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -51,13 +71,16 @@ const Products = () => {
         <div className="col-12">
           <div className="card">
             <div className="card__body">
-            <Table
-                limit= '10'
-                headData={productTableHead}
-                renderHead={(item, index)=> renderHead(item, index)}
-                bodyData={products}
-                renderBody={(item, index)=> renderBody(item, index)}
-              />
+              {loading && <div>Loading</div>}
+              {!loading && (
+                <Table
+                  limit="10"
+                  headData={productTableHead}
+                  renderHead={(item, index) => renderHead(item, index)}
+                  bodyData={data}
+                  renderBody={(item, index) => renderBody(item, index)}
+                />
+              )}
             </div>
           </div>
         </div>
